209394fbc6b37e3c548c1cc9abaaba345d08f52a
GiorgosMandi <giwrgosmandi@gmail.com>
2021-10-08 17:33:13 +0300

    update and cleaning

--- src/main/java/ai/uoa/gr/experiments/lsh/SimpleExp.java
+++ src/main/java/ai/uoa/gr/experiments/lsh/SimpleExp.java
@@ -8,6 +8,7 @@ import ai.uoa.gr.performance.LshPerformance;
 import ai.uoa.gr.utils.Reader;
 import ai.uoa.gr.utils.Utilities;
 import org.apache.commons.cli.*;
+import org.apache.commons.lang3.ArrayUtils;
 import org.scify.jedai.datamodel.EntityProfile;
 import org.scify.jedai.datamodel.IdDuplicates;
 
@@ -18,6 +19,108 @@ import java.util.*;
  */
 public class SimpleExp {
 
+
+    /*
+
+No intersection between: 972 and 8
+No intersection between: 1715 and 13
+No intersection between: 1687 and 23
+No intersection between: 839 and 26
+No intersection between: 660 and 35
+No intersection between: 735 and 35
+No intersection between: 1237 and 35
+No intersection between: 1722 and 35
+No intersection between: 2447 and 35
+No intersection between: 2183 and 56
+No intersection between: 839 and 57
+No intersection between: 839 and 59
+No intersection between: 2183 and 61
+No intersection between: 0 and 62
+No intersection between: 88 and 62
+No intersection between: 131 and 62
+No intersection between: 192 and 62
+No intersection between: 203 and 62
+No intersection between: 276 and 62
+No intersection between: 288 and 62
+No intersection between: 313 and 62
+No intersection between: 331 and 62
+No intersection between: 333 and 62
+No intersection between: 389 and 62
+No intersection between: 396 and 62
+No intersection between: 403 and 62
+No intersection between: 453 and 62
+No intersection between: 489 and 62
+No intersection between: 524 and 62
+No intersection between: 546 and 62
+No intersection between: 601 and 62
+No intersection between: 605 and 62
+No intersection between: 634 and 62
+No intersection between: 656 and 62
+No intersection between: 670 and 62
+No intersection between: 714 and 62
+No intersection between: 715 and 62
+No intersection between: 767 and 62
+No intersection between: 794 and 62
+No intersection between: 810 and 62
+No intersection between: 813 and 62
+No intersection between: 861 and 62
+No intersection between: 878 and 62
+No intersection between: 928 and 62
+No intersection between: 1006 and 62
+No intersection between: 1009 and 62
+No intersection between: 1016 and 62
+No intersection between: 1074 and 62
+No intersection between: 1079 and 62
+No intersection between: 1084 and 62
+No intersection between: 1090 and 62
+No intersection between: 1128 and 62
+No intersection between: 1143 and 62
+No intersection between: 1179 and 62
+No intersection between: 1207 and 62
+No intersection between: 1208 and 62
+No intersection between: 1225 and 62
+No intersection between: 1283 and 62
+No intersection between: 1319 and 62
+No intersection between: 1332 and 62
+No intersection between: 1345 and 62
+No intersection between: 1419 and 62
+No intersection between: 1491 and 62
+No intersection between: 1497 and 62
+No intersection between: 1637 and 62
+No intersection between: 1718 and 62
+No intersection between: 1740 and 62
+No intersection between: 1870 and 62
+No intersection between: 1871 and 62
+No intersection between: 1889 and 62
+No intersection between: 1908 and 62
+No intersection between: 1917 and 62
+No intersection between: 1928 and 62
+No intersection between: 1948 and 62
+No intersection between: 1983 and 62
+No intersection between: 2032 and 62
+No intersection between: 2042 and 62
+No intersection between: 2184 and 62
+No intersection between: 2262 and 62
+No intersection between: 2263 and 62
+No intersection between: 2411 and 62
+No intersection between: 2465 and 62
+No intersection between: 2528 and 62
+No intersection between: 972 and 76
+No intersection between: 839 and 78
+No intersection between: 546 and 83
+No intersection between: 839 and 86
+No intersection between: 35 and 92
+No intersection between: 839 and 92
+No intersection between: 1109 and 92
+No intersection between: 839 and 95
+No intersection between: 543 and 100
+No intersection between: 630 and 100
+No intersection between: 927 and 100
+No intersection between: 1171 and 100
+No intersection between: 1417 and 100
+No intersection between: 2499 and 100
+
+     */
     public static void main(String[] args) {
         try {
 
@@ -46,9 +149,9 @@ public class SimpleExp {
             if (useSuperBit) System.out.println("Using LSH SuperBit");
             else System.out.println("Using LSH MinHash");
 
-            int n = Integer.parseInt(cmd.getOptionValue("ngrams", "1"));
-            int bands = Integer.parseInt(cmd.getOptionValue("bands", "9"));
-            int buckets = Integer.parseInt(cmd.getOptionValue("buckets", "25"));
+            int n = Integer.parseInt(cmd.getOptionValue("ngrams", "2"));
+            int bands = Integer.parseInt(cmd.getOptionValue("bands", "8"));
+            int buckets = Integer.parseInt(cmd.getOptionValue("buckets", "20"));
             System.out.println("N: "+n);
             System.out.println("Bands: "+bands);
             System.out.println("Buckets: "+buckets);
@@ -87,34 +190,18 @@ public class SimpleExp {
             // create models
             System.out.println("Starts Vectorization");
             ShinglingModel model = new ShinglingModel(sourceSTR, n);
-//            int[][] sourceVectorsInt = model.vectorization(sourceSTR);
-//            double[][] sVectors = new double[sourceVectorsInt.length][];
-//            for (int row = 0; row < sourceVectorsInt.length; row++) {
-//                sVectors[row] = Arrays.stream(sourceVectorsInt[row]).asDoubleStream().toArray();
-//            }
-//
-//            int[][] targetVectorsInt = model.vectorization(targetSTR);
-//            double[][] tVectors = new double[targetVectorsInt.length][];
-//            for (int row = 0; row < targetVectorsInt.length; row++) {
-//                tVectors[row] = Arrays.stream(targetVectorsInt[row]).asDoubleStream().toArray();
-//            }
-//            System.out.println("Vectorization Completed\nVector Size:\t"+ model.getVectorSize());
-
-
-            // TODO REMOVE
-            int MAX_SOURCE=10000;
             int[][] sourceVectorsInt = model.vectorization(sourceSTR);
-            double[][] sVectors = new double[MAX_SOURCE][];
-            for (int row = 0; row < MAX_SOURCE; row++) {
+            double[][] sVectors = new double[sourceVectorsInt.length][];
+            for (int row = 0; row < sourceVectorsInt.length; row++) {
                 sVectors[row] = Arrays.stream(sourceVectorsInt[row]).asDoubleStream().toArray();
             }
-            int MAX_TARGET=2000;
+
             int[][] targetVectorsInt = model.vectorization(targetSTR);
-            double[][] tVectors = new double[MAX_TARGET][];
-            for (int row = 0; row <MAX_TARGET ; row++) {
+            double[][] tVectors = new double[targetVectorsInt.length][];
+            for (int row = 0; row < targetVectorsInt.length; row++) {
                 tVectors[row] = Arrays.stream(targetVectorsInt[row]).asDoubleStream().toArray();
             }
-
+            System.out.println("Vectorization Completed\nVector Size:\t"+ model.getVectorSize());
 
             long time = Calendar.getInstance().getTimeInMillis();
             // initialize LSH
@@ -124,71 +211,32 @@ public class SimpleExp {
             else
                 lsh = new MinHash(sVectors, bands, buckets, model.getVectorSize());
 
-
-            // TODO REMOVE
-            int id1 = 8423;
-            int id2 = 9;
-            String entity1 = sourceSTR.get(id1);
-            String entity2 = targetSTR.get(id2);
-            double[] vector1 = sVectors[id1];
-            double[] vector2 = tVectors[id2];
-            Set<String> ngrams1 = model.getNGrams(entity1, n);
-            Set<String> ngrams2 = model.getNGrams(entity2, n);
-            Set<String> intersection12 = new HashSet<>(ngrams1);
-            intersection12.retainAll(ngrams2);
-            int[] bucket1 = lsh.hash(sVectors[id1]);
-            int[] bucket2 = lsh.hash(tVectors[id2]);
-            List<Integer> commonBands = new LinkedList<>();
-            for (int i=0; i<bands;i++){
-                if (bucket1[i] == bucket2[i])
-                    commonBands.add(i);
-            }
-
-            int rows = vector1.length/bands;
-            for (int i: commonBands){
-                List<Integer> subVector1 = new LinkedList<>();
-                List<Integer> subVector2 = new LinkedList<>();
-                List<Integer> ones1 = new LinkedList<>();
-                List<Integer> ones2 = new LinkedList<>();
-                int start = rows*i;
-                int end = rows*(i+1);
-               for (int j=start;j<end;j++){
-                   if (vector1[j] == 1)
-                       ones1.add(j);
-                   if (vector2[j] == 1)
-                       ones2.add(j);
-                   subVector1.add((int)vector1[j]);
-                   subVector2.add((int)vector2[j]);
-               }
-                System.out.println();
-
-            }
-            System.out.println();
-
-
             // true positive
             long tp = 0;
 
+            // false positive pairs
+            long fp = 0;
             // total verifications
             long verifications = 0;
 
             // for each target entity, find its candidates (query)
             // find TP by searching the pairs in GT
-            for (int j=0; j<targetEntities.size(); j++) {
+            for (int j=0; j<tVectors.length; j++) {
                 double[] vector = tVectors[j];
+                String targetStr = targetSTR.get(j);
+                Set<String> targetNGrams = model.getNGrams(targetStr, n);
+
                 Set<Integer> candidates = lsh.query(vector);
                 for (Integer c : candidates) {
 
-                    // TODO check if candidate has common n-grams
+                    // check if candidate has common n-grams
                     String sourceStr = sourceSTR.get(c);
-                    String targetStr = targetSTR.get(j);
-
                     Set<String> sourceNGrams = model.getNGrams(sourceStr, n);
-                    Set<String> targetNGrams = model.getNGrams(targetStr, n);
                     Set<String> intersection = new HashSet<>(sourceNGrams);
                     intersection.retainAll(targetNGrams);
                     if (intersection.isEmpty()){
-                        System.out.println("No intersection between: " + c + " and " + j);
+                        fp += 1;
+                        // continue
                     }
 
                     IdDuplicates pair = new IdDuplicates(c, j);
@@ -207,6 +255,7 @@ public class SimpleExp {
             perf.conditionalUpdate(recall, precision, f1, bands, buckets, verifications, tp, time);
             perf.print();
             System.out.println("Reduction: " + (1- ((float) verifications/cartesian)));
+            System.out.println("fp: " + fp);
 
         } catch (ParseException e) {
             e.printStackTrace();
